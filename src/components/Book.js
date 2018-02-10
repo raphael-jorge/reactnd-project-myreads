20f8b1d160e55117f0956c06680bc0f96cfa162a
raphael-jorge <raphaeljjj@gmail.com>
2018-02-09 22:09:52 -0200

    fix: Allow book component to handle missing data

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,6 +8,10 @@ class Book extends Component {
     availableBookshelves: PropTypes.array.isRequired
   }
 
+  static coverWidth = 128
+  static coverHeight = 193
+  static noImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg'
+
   render () {
     const {bookData, onBookUpdate, availableBookshelves} = this.props;
 
@@ -17,15 +21,25 @@ class Book extends Component {
         <div className='book-top'>
           <div
             className='book-cover'
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${bookData.imageLinks.thumbnail})`
-            }}
+            style={ ( () => {
+              const style = {
+                width: Book.coverWidth,
+                height: Book.coverHeight
+              };
+              if (bookData.imageLinks && bookData.imageLinks.thumbnail) {
+                style.backgroundImage = `url(${bookData.imageLinks.thumbnail})`;
+              } else {
+                style.backgroundImage = `url(${Book.noImageUrl})`;
+                style.backgroundSize = Book.coverWidth;
+                style.backgroundRepeat = 'no-repeat';
+                style.backgroundPosition = 'center';
+              }
+              return style;
+            })() }
           />
           <div className='book-shelf-changer'>
             <select
-              value={bookData.shelf}
+              value={bookData.shelf || 'none'}
               onChange={ event => onBookUpdate(bookData, event.target.value) }
             >
               <option value="none" disabled>Move to...</option>
@@ -39,9 +53,11 @@ class Book extends Component {
           </div>
         </div>
 
-        <div className='book-title'>{bookData.title}</div>
+        {bookData.title &&
+          <div className='book-title'>{bookData.title}</div>
+        }
 
-        {bookData.authors.map( author => (
+        {bookData.authors && bookData.authors.map( author => (
           <div key={author} className='book-authors'>{author}</div>
         ))}
 
